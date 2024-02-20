856283d79caa22a07bd212a833044df549e3b326
cody <codylamy0@gmail.com>
2024-02-20 16:54:27 -0700

    Added Theme element

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-
 import './App.css';
+import styled from "styled-components";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`
 
 function App() {
   return (
